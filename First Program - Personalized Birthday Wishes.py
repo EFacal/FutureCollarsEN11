1785c1de1f1582f6440aed7c0f9a232b4d47169b
Eduardo <efacal@amazon.com>
2025-11-07 18:02:44 +0100

    First commit

---
+++ First Program - Personalized Birthday Wishes.py
@@ -0,0 +1,14 @@
+recipient_name = input("What is your name?")
+year_of_birth = int(input("What is your year of birth?"))
+current_year = 2025
+age = int(current_year - year_of_birth)
+personalized_message = "Cheers to you for another trip around the sun!"
+sender_name = "Eduardo"
+print(f"{recipient_name}, let's celebrate your {age} years of awesomeness!")
+print(f"Wishing you a day filled with joy and laughter as you turn {age}!")
+print("  ")
+print("  ")
+print(f"{personalized_message}")
+print("  ")
+print("  ")
+print(f"With love and best wishes, {sender_name}")
